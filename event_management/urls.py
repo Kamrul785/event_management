57a3ef31c05f4e1b1509fe522641e4e655f0d3f8
Kamrul Hasan <kamrulkhan526785@gmail.com>
2026-03-17 13:25:57 +0600

    Deployment Issue fixed-2

--- event_management/urls.py
+++ event_management/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path,include
 from core.views import  no_permission ,home
-from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -14,5 +13,9 @@ urlpatterns = [
 ] 
  
 if settings.DEBUG:
-    urlpatterns += debug_toolbar_urls()
+    try:
+        from debug_toolbar.toolbar import debug_toolbar_urls
+        urlpatterns += debug_toolbar_urls()
+    except ImportError:
+        pass
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
