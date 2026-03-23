7a288e5443b83fb051639f967e783610e0816c8d
Kamrul Hasan <kamrulkhan526785@gmail.com>
2026-03-23 20:29:16 +0600

    about and 404 page updated

--- core/views.py
+++ core/views.py
@@ -5,5 +5,18 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
 def no_permission(request):
     return render(request, 'no_permission.html')
+
+
+def custom_404(request, exception):
+    return render(request, '404.html', status=404)
--- event_management/urls.py
+++ event_management/urls.py
@@ -1,16 +1,20 @@
 from django.contrib import admin
 from django.urls import path,include
-from core.views import  no_permission ,home
+from core.views import no_permission, home, about, contact, custom_404
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
     path('events/',include("events.urls")),
     path('users/',include("users.urls")),
     path('no_permission/', no_permission, name='no_permission')
 ] 
+
+handler404 = 'core.views.custom_404'
  
 if settings.DEBUG:
     try:
